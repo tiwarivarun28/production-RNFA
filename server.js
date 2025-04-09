30a81152518cd04a2c1eb0f5f5c43493e2e950ac
tiwarivarun28 <117498408+tiwarivarun28@users.noreply.github.com>
2025-04-09 16:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.use(morgan("dev"));
 
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
+//home
+app.get("/",(req,res)=>{
+  res.status(200).send({
+  "success":true,
+    "message":"Node server running"
+  })
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
